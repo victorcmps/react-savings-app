995a19a0172812d62ef347d0ec622ffd1f8b6e93
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2022-12-10 09:49:39 -0300

    feat(components): receive children content in Card to work like a wrapper

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,5 +1,19 @@
 import { ReactElement } from 'react';
+import styled from 'styled-components';
+import { ColorTokens } from '../../styles/colors';
 
-export function Card(): ReactElement {
-  return <h1>Hai</h1>;
+type CardProps = {
+  children: React.ReactNode;
+};
+
+const CardStyle = styled.div`
+  background-color: ${ColorTokens.neutralWhite};
+  box-shadow: 0px 16px 32px rgba(30, 42, 50, 0.08);
+  border-radius: 8px;
+  width: 560px;
+  padding: 0 40px;
+`;
+
+export function Card(props: CardProps): ReactElement {
+  return <CardStyle>{props.children}</CardStyle>;
 }
