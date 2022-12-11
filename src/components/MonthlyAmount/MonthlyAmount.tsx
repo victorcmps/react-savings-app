fcb1162ed82951205d1890e97659418d20a315f3
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2022-12-11 13:50:58 -0300

    feat(components): receives amount, calculates and show the formatted value

--- src/components/MonthlyAmount/MonthlyAmount.tsx
+++ src/components/MonthlyAmount/MonthlyAmount.tsx
@@ -1,38 +1,38 @@
 import { ReactElement } from 'react';
-import { formatMonth } from '../../utils/dates';
-import {
-  AmountInfo,
-  StyledMonthlyAmount,
-  Title,
-  Amount,
-  AmountDetail,
-  AmountDetailText,
-} from './MonthlyAmount.style';
+import { formatCurrency, formatMonth } from '../../utils';
+import * as SC from './MonthlyAmount.style';
 
 type MonthlyAmountProps = {
   date: Date;
-  amount?: number;
+  amount: number;
   monthCounter: number;
 };
 
 export function MonthlyAmount(props: MonthlyAmountProps): ReactElement {
+  const calcMonthlyAmount = (amount: number, monthCounter: number) => {
+    const calc = amount / monthCounter;
+    return formatCurrency(calc);
+  };
+
   return (
-    <StyledMonthlyAmount>
-      <AmountInfo>
-        <Title>Monthly amount</Title>
-        <Amount>$520.83</Amount>
-      </AmountInfo>
-      <AmountDetail>
-        <AmountDetailText>
+    <SC.StyledMonthlyAmount>
+      <SC.AmountInfo>
+        <SC.Title>Monthly amount</SC.Title>
+        <SC.Amount>
+          ${calcMonthlyAmount(props.amount, props.monthCounter)}
+        </SC.Amount>
+      </SC.AmountInfo>
+      <SC.AmountDetail>
+        <SC.AmountDetailText>
           You&apos;re planning{' '}
           <strong>{props.monthCounter} monthly deposits</strong> to reach your{' '}
-          <strong>$25,000 goal</strong> by{' '}
+          <strong>${formatCurrency(props.amount)} goal</strong> by{' '}
           <strong>
             {formatMonth(props.date)} {props.date.getFullYear()}
           </strong>
           .
-        </AmountDetailText>
-      </AmountDetail>
-    </StyledMonthlyAmount>
+        </SC.AmountDetailText>
+      </SC.AmountDetail>
+    </SC.StyledMonthlyAmount>
   );
 }
