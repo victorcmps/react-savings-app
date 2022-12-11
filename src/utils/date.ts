a6ebfe776d488ef3a8325ad61f2002cedd015b8f
João Victor Pires Almeida Campos <joao.victor_55@hotmail.com>
2022-12-11 13:41:50 -0300

    feat(utils): add JSDoc comments

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,20 @@
+/**
+ * Get the full month name of a date.
+ * @param {Date} date - The date
+ * @returns {string} Returns the name
+ */
 export function formatMonth(date: Date): string {
   return date.toLocaleString('en-US', { month: 'long' });
 }
 
+/**
+ * Get the next or the previous month of a date.
+ * @param {Date} date - The date
+ * @param {boolean} isPreviousMonth - Tells the method whether to look for the previous date
+ * @returns {string} Returns the full date
+ */
 export function getNextOrPrevDate(date: Date, isPreviousMonth: boolean): Date {
-  // Verify if date reached the 12 or 1 month (if clicked on previous) to advance to january or get back to december respectively
+  /**  Verify if date reached December or January (if clicked on previous) to advance to January or get back to December respectively */
   const maxMonth = isPreviousMonth ? 0 : 11;
   if (date.getMonth() === maxMonth) {
     return isPreviousMonth
--- src/utils/input.ts
+++ src/utils/input.ts
@@ -1,20 +1,34 @@
+/**
+ * Remove characters and punctuation from given string
+ * @param {number} value - The number
+ * @returns {string} Returns formatted value with only numbers
+ */
 export function formatOnlyNumbers(value: string): string {
   const regex = /[^0-9]/g;
   return value.replace(regex, '');
 }
 
+/**
+ * Format a number to US Currency
+ * @param {number} value - The number
+ * @returns {string} Returns formatted value
+ */
 export function formatCurrency(value: number): string {
   const dollarLocale = Intl.NumberFormat('en-US', {
     minimumFractionDigits: 0,
     maximumFractionDigits: 2,
     currency: 'USD',
   });
-  const format = dollarLocale.format(value);
-  // This add a additional 0 if decimal has less than 2 numbers in order to present the complete value to the user.
-  const addDecimal = checkDecimal(format) ? format + '0' : format;
-  return addDecimal;
+  const currency = dollarLocale.format(value);
+  const formattedValue = checkDecimal(currency) ? currency + '0' : currency;
+  return formattedValue;
 }
 
+/**
+ * Given a number, verifies if the decimal has less than 2 numbers.
+ * @param {string} value - The number
+ * @returns {boolean} Return ´true´ if decimal has less than 2 numbers, and ´false´ otherwise.
+ */
 function checkDecimal(value: string): boolean {
   const decimal = value.split('.')[1];
   return decimal && decimal.length < 2 ? true : false;
